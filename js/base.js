346287a040373d7c8edc4bfa42580538df4a89c3
CalicoCatio <132619375+CalicoCatio@users.noreply.github.com>
2025-07-16 14:00:09 +0200

    Update base.js

--- js/base.js
+++ js/base.js
@@ -171,7 +171,7 @@ function insertFooter() {
 					</div>
 					<span class="d-flex justify-content-end align-items-end">
 						<span class="footer-text">
-							<span id="myVer">v0.2.2</span>
+							<span id="myVer">v0.2.2a</span>
 						</span>
 					</span>
 				</div>
