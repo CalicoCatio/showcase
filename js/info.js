13b506444bbe17eb284100da141249a51a6f5de6
DogDoge0-0 <arielmena167@gmail.com>
2025-07-22 16:01:35 +0200

    oops x4

--- js/info.js
+++ js/info.js
@@ -3,25 +3,35 @@ function scrollToBottom() {
     if (!isActive) {
         isActive = true;
         const info = document.querySelector('#infoHeader');
-        
+        const { top, left, bottom, right } = info.getBoundingClientRect();
+        const { innerHeight, innerWidth } = window;
         
         if (info) {
-            info.scrollIntoView({ behavior: 'smooth' });
-            const handler1 = () => {
-
-                window.removeEventListener("scrollend", handler1);
-                
-
+            if ((top > 0 && top < innerHeight) && (left > 0 && left < innerWidth) && (bottom > 0 && bottom < innerHeight) && (right > 0 && right < innerWidth)) {
                 info.classList.add('highlight-anim');
 
                 const handler2 = () => {
-                    window.removeEventListener("scrollend", handler2);
+                    window.removeEventListener("animationend", handler2);
                     info.classList.remove('highlight-anim');
                     isActive = false;
                 }
                 addEventListener("animationend", handler2);
+            } else {
+                info.scrollIntoView({ behavior: 'smooth' });
+                const handler1 = () => {
+                    window.removeEventListener("scrollend", handler1);
+
+                    info.classList.add('highlight-anim');
+
+                    const handler2 = () => {
+                        window.removeEventListener("animationend", handler2);
+                        info.classList.remove('highlight-anim');
+                        isActive = false;
+                    }
+                    addEventListener("animationend", handler2);
+                }
+                addEventListener("scrollend", handler1);
             }
-            addEventListener("scrollend", handler1);
         }
     }
 }
